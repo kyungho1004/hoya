d8aaf035f6654a2f4066eb6d2847c9302b09d09a
kyungho1004 <lee729812@gmail.com>
2025-09-01 09:17:39 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,5 @@
 
-import sys
-import os
-
-# bloodmap_app 경로를 강제로 인식시켜줌
-sys.path.append(os.path.join(os.path.dirname(__file__), "bloodmap_app"))
-
-# streamlit_main.py 안의 main() 호출
-from streamlit_main import main
+from app import main
 
 if __name__ == "__main__":
     main()
