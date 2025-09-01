56ffe85104289774b5fdc5800baf8bf5838a411b
kyungho1004 <lee729812@gmail.com>
2025-09-01 09:10:54 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
-from bloodmap_app.streamlit_main import main
+import sys
+import os
+
+# 현재 파일 기준으로 bloodmap_app 폴더 경로를 시스템 경로에 추가
+sys.path.append(os.path.join(os.path.dirname(__file__), "bloodmap_app"))
+
+from streamlit_main import main
 
 if __name__ == "__main__":
     main()
