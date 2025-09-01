425aab5a8f234ce7c88901fea7753ceda86c49e7
kyungho1004 <lee729812@gmail.com>
2025-09-01 14:52:14 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title='피수치 가이드')
+st.title('피수치 가이드')
+st.write('모듈 로드 완료')
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,1 @@
-
-# -*- coding: utf-8 -*-
-import streamlit as st
-from bloodmap_app.app import main
-
-st.set_page_config(page_title="피수치 가이드 (v3.14-fixed)", page_icon="🩸", layout="centered")
-
-if __name__ == "__main__":
-    main()
+import app
