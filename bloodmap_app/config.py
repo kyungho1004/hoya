2f149ff971a88ef52844b3111fcafca490cd9574
kyungho1004 <lee729812@gmail.com>
2025-09-02 14:08:49 +0900

    Add files via upload

--- bloodmap_app/config.py
+++ bloodmap_app/config.py
@@ -6,7 +6,6 @@ FOOTER_CAFE = "공식 카페: 피수치 가이드 공식카페"
 DISCLAIMER = "본 자료는 보호자의 이해를 돕기 위한 참고 정보이며, 모든 의학적 판단과 치료는 담당 의료진의 권한입니다."
 FEVER_GUIDE = "38.0~38.5℃: 해열제/경과관찰 · 38.5℃ 이상: 병원 연락 · 39℃ 이상: 즉시 병원 방문 권고"
 
-# Bilingual labels
 LBL_WBC="WBC (백혈구)"; LBL_Hb="Hb (혈색소)"; LBL_PLT="PLT (혈소판)"; LBL_ANC="ANC (호중구/면역력)"
 LBL_Ca="Ca (칼슘)"; LBL_P="P (인)"; LBL_Na="Na (나트륨)"; LBL_K="K (칼륨)"
 LBL_Alb="Albumin (알부민)"; LBL_Glu="Glucose (혈당)"; LBL_TP="Total Protein (총단백질)"
@@ -16,4 +15,5 @@ LBL_Cr="Cr (신장수치)"; LBL_UA="Uric Acid (요산수치)"; LBL_TB="Total Bil
 ORDER = [LBL_WBC, LBL_Hb, LBL_PLT, LBL_ANC, LBL_Ca, LBL_P, LBL_Na, LBL_K, LBL_Alb,
          LBL_Glu, LBL_TP, LBL_AST, LBL_ALT, LBL_LDH, LBL_CRP, LBL_Cr, LBL_UA, LBL_TB, LBL_BUN, LBL_BNP]
 
-FONT_PATH_REG = "bloodmap_app/fonts/NanumGothic-Regular.ttf"
+# Use NanumGothic.ttf for maximum compatibility
+FONT_PATH_REG = "bloodmap_app/fonts/NanumGothic.ttf"
