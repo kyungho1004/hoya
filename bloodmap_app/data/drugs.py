9233f9e97bb41f2dc462a65b6fe9bad88d23d615
kyungho1004 <lee729812@gmail.com>
2025-09-02 15:57:53 +0900

    Add files via upload

--- bloodmap_app/data/drugs.py
+++ bloodmap_app/data/drugs.py
@@ -1,33 +1,33 @@
-# -*- coding: utf-8 -*-
-# 최소 동작을 위한 샘플(실사용엔 더 많은 약물 사전이 연결됨)
+
 ANTICANCER = {
-    "ATRA": {"alias": "베사노이드(트레티노인)", "aes": ["분화증후군","피부 건조","두통","간수치 상승","설사"]},
-    "ARA-C": {"alias": "시타라빈", "aes": ["골수억제","발열","오심","고용량: 소뇌증상"]},
-    "MTX": {"alias": "메토트렉세이트", "aes": ["간독성","구내염","신독성(고용량)","광과민"]},
-    "G-CSF": {"alias": "그라신", "aes": ["골통","미열","백혈구 상승"]},
-    "Cyclophosphamide": {"alias": "사이클로포스파마이드", "aes": ["골수억제","출혈성 방광염"]},
+    "MTX": {"alias": "메토트렉세이트", "aes": ["간독성","구내염","골수억제","신장독성"]},
+    "ATRA": {"alias": "베사노이드(트레티노인)", "aes": ["분화증후군","두통","피부건조"]},
+    "ARA-C": {"alias": "시타라빈", "aes": ["골수억제","결막염","신경독성(고용량)"]},
+    "G-CSF": {"alias": "그라신", "aes": ["골통","발열"]},
+    "Cyclophosphamide": {"alias": "사이클로포스파마이드", "aes": ["오심구토","출혈성 방광염"]},
     "Etoposide": {"alias": "에토포사이드", "aes": ["골수억제","저혈압(주입)"]},
-    "Daunorubicin": {"alias": "다우노루비신", "aes": ["심독성","탈모","점막염"]},
-    "Idarubicin": {"alias": "이다루비신", "aes": ["심독성","골수억제"]},
-    "Fludarabine": {"alias": "플루다라빈", "aes": ["면역억제","감염 위험"]},
-    "Imatinib": {"alias": "이미티닙", "aes": ["부종","근육통","간수치"]},
-    "Sorafenib": {"alias": "소라페닙", "aes": ["수족증후군","고혈압","피로"]},
-    "Bevacizumab": {"alias": "베바시주맙", "aes": ["출혈","혈전","상처치유 지연"]},
-    "Pembrolizumab": {"alias": "펨브롤리주맙", "aes": ["면역관련 이상반응(피부/간/폐)"]},
+    "Fludarabine": {"alias": "플루다라빈", "aes": ["면역억제"]},
+    "Hydroxyurea": {"alias": "하이드록시우레아", "aes": ["골수억제"]},
+    "Daunorubicin": {"alias": "도우노루비신", "aes": ["심독성","탈모"]},
+    "Idarubicin": {"alias": "이달루비신", "aes": ["심독성"]},
+    "Imatinib": {"alias": "이미티닙", "aes": ["부종","근육통"]},
+    "Dasatinib": {"alias": "다사티닙", "aes": ["흉막삼출","혈소판감소"]},
+    "Nilotinib": {"alias": "닐로티닙", "aes": ["QT 연장"]},
+    "Vincristine": {"alias": "빈크리스틴", "aes": ["말초신경병증"]},
+    "Asparaginase": {"alias": "아스파라기나제", "aes": ["췌장염","혈전"]},
+    "Topotecan": {"alias": "토포테칸", "aes": ["골수억제"]},
+    "Docetaxel": {"alias": "도세탁셀", "aes": ["무과립구열","말초신경병증"]},
+    "Carboplatin": {"alias": "카보플라틴", "aes": ["골수억제"]},
+    "Cisplatin": {"alias": "시스플라틴", "aes": ["신독성","구토"]},
+    "Paclitaxel": {"alias": "파클리탁셀", "aes": ["말초신경병증"]},
+    "Bevacizumab": {"alias": "베바시주맙", "aes": ["고혈압","출혈"]},
+    "Pembrolizumab": {"alias": "펨브롤리주맙", "aes": ["면역관련 이상반응"]},
     "Nivolumab": {"alias": "니볼루맙", "aes": ["면역관련 이상반응"]},
 }
 
 ABX_GUIDE = {
-    "퀴놀론(Levo/Moxi 등)": [
-        "힘줄염/파열 주의", "QT 연장 가능", "임신/소아 신중"
-    ],
-    "마크롤라이드(Azithro/Clarithro)": [
-        "CYP 상호작용", "QT 연장", "위장관 자극"
-    ],
-    "세팔로스포린": [
-        "1~4세대 폭넓은 커버리지", "과민반응 시 주의"
-    ],
-    "페니실린/베타락탐": [
-        "발진·아나필락시스 가능", "항응고제 상호작용 주의"
-    ]
+    "Quinolone(퀴놀론)": ["QT 연장", "광과민", "힘줄염/파열 드묾"],
+    "Macrolide(마크롤라이드)": ["CYP3A4 상호작용", "QT 연장"],
+    "Penicillin/βLactam": ["아나필락시스 병력 주의", "설사·발진 가능"],
+    "Cephalosporin": ["과민반응 병력 확인", "일부 약물 알코올 상호작용"],
 }
