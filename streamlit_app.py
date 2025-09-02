739bf86dc5a9341afe7efb9183fb6c496176e247
kyungho1004 <lee729812@gmail.com>
2025-09-02 15:17:14 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-import sys, os
-# Ensure project root is first. This prevents picking up any globally installed bloodmap_app.
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-if BASE_DIR not in sys.path:
-    sys.path.insert(0, BASE_DIR)
-
 from bloodmap_app.app import main
-
 if __name__ == "__main__":
     main()
