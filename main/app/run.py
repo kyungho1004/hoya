ce54fe408f356b305efa3a2dc19afe0f2d5a9d65
kyungho1004 <lee729812@gmail.com>
2025-09-03 13:13:42 +0900

    Add files via upload

--- main/app/run.py
+++ main/app/run.py
@@ -1,1 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Compatibility entrypoint: main/app/run.py
+Many managed hosts look for this path by default.
+"""
+from bloodmap_app.app import main
 
+if __name__ == "__main__":
+    main()
