28b445e62ecec8bf2bee3622011779c123817b00
kyungho1004 <lee729812@gmail.com>
2025-09-03 13:35:30 +0900

    Add files via upload

--- bloodmap/app_user/__init__.py
+++ bloodmap/app_user/__init__.py
@@ -1,1 +1,3 @@
-
+# -*- coding: utf-8 -*-
+# alias: import bloodmap.app_user → bloodmap_app
+from bloodmap_app import *
---
+++ bloodmap/app_user/main.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from bloodmap_app.app import main
+if __name__ == "__main__":
+    main()
