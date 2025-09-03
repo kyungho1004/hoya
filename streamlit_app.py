323493b7f4de0a133b1aadf00b5266ccec9f837d
kyungho1004 <lee729812@gmail.com>
2025-09-03 10:48:56 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
-
-import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "bloodmap_app")))
-
-from app import main
+import streamlit as st
+from bloodmap_app.app import main
 
 if __name__ == "__main__":
     main()
